4ebb00621565873cc6c7e54e233d878f030f6477
yusufzngnn <yusufzngn@hotmail.com>
2024-11-07 12:23:49 +0300

    Update KarakterKontrol.cs

--- Assets/KarakterKontrol.cs
+++ Assets/KarakterKontrol.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class KarakterKontrol : MonoBehaviour
 {
-    // Ad Soyad: 
-    // Öğrenci Numarası: 
+    // Ad Soyad: Yusuf Zengin
+    // Öğrenci Numarası: 232011045
 
 
-    // Soru 1: Karakteri yön tuşları ile hareket ettiren kodu, HareketEt fonksiyonu içerisine yazınız.
-    // Soru 2: Karakterin zıplamasını sağlaması beklenen Zipla metodu doğru bir şekilde çalışmıyor, koddaki hatayı düzeltin.
-    // Soru 3: Karakterin 'Engel' tag'ine sahip objeye temas ettiğinde metin objesine "Oyun Bitti!" yazısını yazdırınız.
-    // Soru 4: Karakterin 'Puan' tag'ine sahip objeye temas ettiğinde skoru 1 arttırınız ve metin objesine yazdırınız.
+    //+ Soru 1: Karakteri yön tuşları ile hareket ettiren kodu, HareketEt fonksiyonu içerisine yazınız.
+    //+ Soru 2: Karakterin zıplamasını sağlaması beklenen Zipla metodu doğru bir şekilde çalışmıyor, koddaki hatayı düzeltin.
+    //+ Soru 3: Karakterin 'Engel' tag'ine sahip objeye temas ettiğinde metin objesine "Oyun Bitti!" yazısını yazdırınız.
+    //+ Soru 4: Karakterin 'Puan' tag'ine sahip objeye temas ettiğinde skoru 1 arttırınız ve metin objesine yazdırınız.
 
     // Not: Engel ve Puan nesnelerinin isTrigger özelliği aktiftir.
 
@@ -32,20 +32,46 @@ public class KarakterKontrol : MonoBehaviour
     void Update()
     {
         // Yazdığınız metodları çağırınız.
+        HareketEt();
+        Zipla();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Soru 3 ve soru 4 burada çözülecek.
-    }
+        if (other.gameObject.CompareTag("Puan"))
+        {
+            skor += 1;
+            Destroy(other.gameObject);
+            metin.text = "score: " + skor;
+        }
 
+        if (other.gameObject.CompareTag("Engel"))
+        {
+            metin.text="Oyun Bitti!"
+            Destroy(other.gameObject);
+
+        }
+    }
+    void HareketEt()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            karakterRb.AddForce(UnityEngine.Vector2.left * (hiz * Time.deltaTime));
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            karakterRb.AddForce(UnityEngine.Vector2.right * (hiz * Time.deltaTime));
+        }
+    }
     void Zipla()
     {
         // Space tuşuna basınca karakter zıplamalı ancak aşağıdaki kod hatalı.
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 ziplamaYonu = new Vector3(UnityEngine.Random.Range(-1f, 1f), 1, UnityEngine.Random.Range(-1f, 1f));
-            karakterRb.AddForce(ziplamaYonu * (ziplamaGucu / 2), ForceMode2D.Impulse);
+            
+           karakterRb.AddForce(UnityEngine.Vector2.up * (ziplamaGucu * Time.deltaTime));
         }
     }
 }
